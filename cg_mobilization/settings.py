fa3327d184456d2c7f2f626a5385c2d9bd932726
jburchel <jburchel@gmail.com>
2024-08-23 11:21:21 -0400

    render updates static stuff

--- cg_mobilization/settings.py
+++ cg_mobilization/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cg_mobilization.urls'
@@ -129,7 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- cg_mobilization/wsgi.py
+++ cg_mobilization/wsgi.py
@@ -9,13 +9,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 path = '/home/cgmobilize/cg_mobilization'
 if path not in sys.path:
     sys.path.append(path)
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cg_mobilization.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
