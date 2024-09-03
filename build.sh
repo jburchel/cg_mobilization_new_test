2210fd5961041de10cc035bdccd53d5bf829d20f
jburchel <jburchel@gmail.com>
2024-09-03 12:28:37 -0400

    Add custom migration command and update build script

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py apply_migrations
+python manage.py apply_userprofile_migration
