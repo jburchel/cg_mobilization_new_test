c1804e58d1123826fbe2a2b46a4b13bda9450468
jburchel <jburchel@gmail.com>
2024-09-03 13:38:59 -0400

    updated some OAuth2 stuff

--- cg_mobilization/settings/production.py
+++ cg_mobilization/settings/production.py
@@ -19,17 +19,17 @@ DATABASES = {
 # Production-specific Google OAuth settings
 GOOGLE_CLIENT_CONFIG = {
     "web": {
-        "client_id": '877634291388-70aa4mv47pomvt76r6jvlqkeaku1h2m8.apps.googleusercontent.com',
-        "project_id": 'crm-application-433417',
+        "client_id": os.environ.get('GOOGLE_CLIENT_ID'),
+        "project_id": os.environ.get('GOOGLE_PROJECT_ID'),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_secret": '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',
-        "redirect_uris": ["https://mobilize.onrender.com/integrations/google/auth/callback/"]
+        "client_secret": os.environ.get('GOOGLE_CLIENT_SECRET'),
+        "redirect_uris": [os.environ.get('GOOGLE_REDIRECT_URI')]
     }
 }
 
-GOOGLE_REDIRECT_URI = "https://mobilize.onrender.com/integrations/google/auth/callback/"
+GOOGLE_REDIRECT_URI = os.environ.get('GOOGLE_REDIRECT_URI')
 GOOGLE_CALENDAR_CREDENTIALS_FILE = BASE_DIR / 'credentials_prod.json'
 GOOGLE_CALENDAR_SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 
