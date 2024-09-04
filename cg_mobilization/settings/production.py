e0f3b8c711e0169d939d27c2215b80aa5cf00c68
jburchel <jburchel@gmail.com>
2024-09-04 10:41:55 -0400

    changes to production settings and calendar authentication

--- cg_mobilization/settings/production.py
+++ cg_mobilization/settings/production.py
@@ -1,5 +1,6 @@
 from .base import *
 import os
+from pathlib import Path
 
 DEBUG = False
 
@@ -30,7 +31,7 @@ GOOGLE_CLIENT_CONFIG = {
 }
 
 GOOGLE_REDIRECT_URI = os.environ.get('GOOGLE_REDIRECT_URI')
-GOOGLE_CALENDAR_CREDENTIALS_FILE = BASE_DIR / 'credentials_prod.json'
+GOOGLE_CALENDAR_CREDENTIALS_FILE = Path(os.getenv('GOOGLE_CREDENTIALS_FILE', 'credentials_prod.json'))
 GOOGLE_CALENDAR_SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 
 # Additional security settings for production
@@ -43,3 +44,8 @@ EMAIL_HOST = 'smtp.gmail.com'  # or your email provider's SMTP server
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+print(f"BASE_DIR: {BASE_DIR}")
+print(f"STATIC_ROOT: {BASE_DIR / 'staticfiles'}")
+print(f"MEDIA_ROOT: {BASE_DIR / 'media'}")
+print(f"Current working directory: {os.getcwd()}")
+print(f"Contents of current directory: {os.listdir('.')}")
