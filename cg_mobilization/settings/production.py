6b47513a79d5bdf90f7ac96165eb727ff4ec4b47
jburchel <jburchel@gmail.com>
2024-10-18 14:53:53 -0400

    collected static

--- cg_mobilization/settings/production.py
+++ cg_mobilization/settings/production.py
@@ -28,6 +28,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Production-specific Google OAuth settings
 GOOGLE_CLIENT_CONFIG = {
     "web": {
