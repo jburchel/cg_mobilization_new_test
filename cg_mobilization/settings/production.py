6dd525504b1cd6086524595859beb5ee67bec1bb
jburchel <jburchel@gmail.com>
2024-10-18 15:07:11 -0400

    updated media storage to Render

--- cg_mobilization/settings/production.py
+++ cg_mobilization/settings/production.py
@@ -29,7 +29,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/opt/render/project/src/media'
 
 # Production-specific Google OAuth settings
 GOOGLE_CLIENT_CONFIG = {
