7b320babf22c3c31a5accb1422bd9ec5c9bce4a9
jburchel <jburchel@gmail.com>
2024-10-25 10:40:55 -0400

    changed wsgi file

--- cg_mobilization/wsgi.py
+++ cg_mobilization/wsgi.py
@@ -8,22 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cg_mobilization.settings.production')
 
 application = get_wsgi_application()
-
-static_root = '/opt/render/project/src/staticfiles'
-application = WhiteNoise(application, root='/opt/render/project/src/staticfiles')
-application.add_files('/opt/render/project/src/static', prefix='static/')
-
-media_root = '/opt/render/project/src/media'
-
-import logging
-logger = logging.getLogger(__name__)
-logger.info(f"Static root path: {static_root}")
-logger.info(f"Media root path: {media_root}")
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media'))
+application.add_files(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media'), prefix='media/')
